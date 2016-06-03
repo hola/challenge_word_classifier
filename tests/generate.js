804cb36f78692c02e0e8e41f3c05f58363d3198b
Alexey Feldgendler <alexey@feldgendler.ru>
2016-06-03 12:54:52 +0300

    Added testcase generator.

---
+++ tests/generate.js
@@ -0,0 +1,75 @@
+// LICENSE_CODE ZON
+'use strict'; /*jslint node:true*/
+const fs = require('fs');
+const rimraf = require('rimraf');
+const mkdir_parents = require('mkdir-parents');
+const generator = require('./generator.js');
+const getopt = require('node-getopt').create([
+    ['w', 'words=WORDLIST', 'wordlist location (default: ../words.txt)'],
+    ['t', 'text=STRING', 'seed string'],
+    ['s', 'start=N', 'starting seed'],
+    ['v', 'verbose', 'log names of files written to'],
+    ['h', 'help', 'display this help'],
+]).setHelp(`Usage: node generate.js [OPTION] N TARGET
+
+[[OPTIONS]]
+
+Either --text or --start must be specified. In the former case, the text
+is used to generate a repeatable pseudo-random sequence of block seeds.
+In the latter case, block seeds start with the specified number and proceed
+in increments of 1.
+
+N: Number of 100-word blocks to generate.
+TARGET: The directory where to put the destcases.
+The TARGET directory will be deleted and re-created!`).bindHelp();
+
+function*straight(n){
+    while (true)
+        yield n++;
+}
+
+function main(){
+    let opt = getopt.parseSystem();
+    let seeds;
+    if ('text' in opt.options && 'start' in opt.options)
+        throw new Error('Conflicting command-line options');
+    if ('text' in opt.options)
+        seeds = generator.sequence(opt.options.text);
+    else if ('start' in opt.options)
+    {
+        let start = +opt.options.start;
+        if (!Number.isFinite(start))
+            throw new Error('N must be a number');
+        seeds = straight(start);
+    }
+    else
+        return getopt.showHelp();
+    if (opt.argv.length!=2)
+        return getopt.showHelp();
+    let wordlist = opt.options.words||'../words.txt';
+    let n = +opt.argv[0], target = opt.argv[1];
+    rimraf.sync(target, {glob: false});
+    mkdir_parents.sync(target);
+    if (opt.options.verbose)
+        console.log(`Initializing with ${wordlist}`);
+    let words = fs.readFileSync(wordlist, 'utf8').slice(0, -1).split('\n');
+    generator.init(words);
+    let i = 1, width = n.toString().length;
+    for (let seed of seeds)
+    {
+        let tc = generator.generate(seed);
+        for (let word in tc)
+            tc[word] = tc[word].real;
+        let name = i.toString();
+        while (name.length<width)
+            name = '0'+name;
+        name = `${target}/${name}-${seed}.json`;
+        if (opt.options.verbose)
+            console.log(name);
+        fs.writeFileSync(name, JSON.stringify(tc, null, 4)+'\n');
+        if (++i>n)
+            break;
+    }
+}
+
+main();
---
+++ tests/generator.js
@@ -0,0 +1,137 @@
+// LICENSE_CODE ZON
+'use strict'; /*jslint node:true*/
+const random_js = require('random-js');
+const _ = require('underscore');
+
+module.exports = {
+    init,
+    generate,
+    sequence,
+};
+
+const CHARS = 28, TC_SIZE = 100, MAX_DEPTH = 8;
+let models, dictionary;
+
+function c2i(c)
+{
+    if (!c)
+        return 0;
+    if (c=="'")
+        return 27;
+    return c.charCodeAt(0)-96;
+}
+
+function i2c(i)
+{
+    if (!i)
+        return '';
+    if (i==27)
+        return "'";
+    return String.fromCharCode(i+96);
+}
+
+class MarkovModel {
+    constructor(order, prev){
+        this.order = order;
+        this.data = new Map();
+        this.prev = prev;
+    }
+    learn(word){
+        for (let i = 0; i<=word.length; i++)
+        {
+            let state = word.slice(Math.max(i-this.order, 0), i);
+            let item = this.data.get(state);
+            if (!item)
+            {
+                item = new Uint32Array(CHARS+1);
+                this.data.set(state, item);
+            }
+            item[0]++;
+            item[c2i(word[i])+1]++;
+        }
+    }
+    produce(random, prefix){
+        let item = this.data.get(prefix.slice(-this.order));
+        if (!item)
+            return this.prev(prefix);
+        let n = random.integer(0, item[0]-1);
+        for (let i = 1; i<=CHARS; i++)
+        {
+            n -= item[i];
+            if (n<0)
+                return i2c(i-1);
+        }
+    }
+    generate(random){
+        let res = '';
+        while (true)
+        {
+            let c = this.produce(random, res);
+            if (!c)
+                return res;
+            res += c;
+        }
+    }
+}
+
+class LengthModel {
+    learn(){}
+    produce(random, prefix){ return i2c(random.integer(0, CHARS-1)); }
+    generate(random){
+        let res = '', limit = random.integer(10, 30);
+        for (let i = 0; i<limit; i++)
+        {
+            let c = this.produce(random, res);
+            if (!c && res)
+                return res;
+            res += c;
+        }
+        return res;
+    }
+}
+
+function init(words){
+    models = [new LengthModel()];
+    for (let i = 1; i<=MAX_DEPTH; i++)
+        models.push(new MarkovModel(i, models[i-1]));
+    dictionary = words.map(w=>w.toLowerCase()).sort();
+    for (let word of dictionary)
+    {
+        for (let model of models)
+            model.learn(word);
+    }
+}
+
+function generate(seed)
+{
+    let random = new random_js(random_js.engines.mt19937().seed(seed));
+    let res = {};
+    for (let i = 0; i<TC_SIZE; i++)
+    {
+        let word, real = random.bool(), model;
+        if (real)
+            word = random.pick(dictionary);
+        else
+        {
+            do {
+                model = random.integer(0, models.length-1);
+                word = models[model].generate(random);
+            } while (_.indexOf(dictionary, word, true)>=0);
+        }
+        if (word in res)
+            i--;
+        else
+            res[word] = {real, model};
+    }
+    return res;
+}
+
+function*sequence(seed_text)
+{
+    let arr = [];
+    for (let i = 0; i<seed_text.length; i++)
+        arr.push(seed_text.charCodeAt(i));
+    let random = new random_js(random_js.engines.mt19937().seedWithArray(arr));
+    while (true)
+        yield random.integer(0, 0x7fffffff);
+}
